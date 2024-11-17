d4c9678c6d227d6d76ca77f93fd2f87a22c5be5e
muratkistan <muratkistan@gmail.com>
2024-11-18 00:05:44 +0300

    Schedule settet 10 seconds

--- product-service/src/main/java/com/muratkistan/product_service/schedule/ProductSchedule.java
+++ product-service/src/main/java/com/muratkistan/product_service/schedule/ProductSchedule.java
@@ -2,17 +2,20 @@ package com.muratkistan.product_service.schedule;
 
 import com.muratkistan.product_service.product.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ProductSchedule {
 
     private final ProductService productService;
 
-    @Scheduled(fixedRate = 3600000)
+    @Scheduled(fixedRate = 10000)
     public void productListRefresh(){
+        log.info("Product List Cache Refreshed");
         productService.getProductListCacheUpdate();
     }
 }
