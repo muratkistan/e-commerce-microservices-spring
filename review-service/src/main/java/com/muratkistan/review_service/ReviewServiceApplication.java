407dbd853d8ab1b3812566aacca60dcff88b41b2
muratkistan <muratkistan@gmail.com>
2024-11-20 01:27:59 +0300

    added gateway, eureka, zipkin and config server configuration

--- review-service/src/main/java/com/muratkistan/review_service/ReviewServiceApplication.java
+++ review-service/src/main/java/com/muratkistan/review_service/ReviewServiceApplication.java
@@ -2,10 +2,12 @@ package com.muratkistan.review_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
 @EnableElasticsearchRepositories
+@EnableDiscoveryClient
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
