b077975b2201ce48d0cdb77e5da7be57d6cb69ac
muratkistan <muratkistan@gmail.com>
2024-12-14 00:09:37 +0300

    mapper bug fixed

--- order-service/src/main/java/com/muratkistan/order_service/orderline/OrderLineMapper.java
+++ order-service/src/main/java/com/muratkistan/order_service/orderline/OrderLineMapper.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 public class OrderLineMapper {
     public OrderLine toOrderLine(OrderLineRequest request) {
         return OrderLine.builder()
-                .id(request.orderId())
                 .productId(request.productId())
                 .order(
                         Order.builder()
